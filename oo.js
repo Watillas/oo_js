30c9263b413b09a42b76c5d0118d34ac97db4ddc
Watillas <watillasmateus554@gmail.com>
2026-03-02 14:17:46 +0000

    Initial commit

---
+++ oo.js
@@ -0,0 +1,17 @@
+class pessoa{
+    constructor(nome, idade, trabalho){
+        this.nome = nome;
+        this.idade = idade;
+        this.trabalho = trabalho;
+    }
+}
+
+ function salario (salario) {
+    this.salario = salario;
+
+    pessoa.call(this, nome, idade , trabalho)
+ }
+
+ const pessoa1 = new pessoa1 (watillas , 23, back-end, 3000)
+ const pessoa2 = new pessoa2 (matheus,18,front-end,2500)
+ const pessoa3 = new pessoa3 (julio,30,motorista,1500)
