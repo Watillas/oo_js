b409f3ff2075f3c71f319c7859a756af1878c300
Watillas <watillasmateus554@gmail.com>
2026-03-02 14:46:45 +0000

    consertando codigo

--- oo.js
+++ oo.js
@@ -6,12 +6,27 @@ class pessoa{
     }
 }
 
- function salario (salario) {
-    this.salario = salario;
+ class funcionario1 extends pessoa{ 
+     constructor (nome, idade, trabalho){
+        super(nome, idade, trabalho)
+        this.salario = salario;
+     }
 
-    pessoa.call(this, nome, idade , trabalho)
  }
 
- const pessoa1 = new pessoa1 (watillas , 23, back-end, 3000)
- const pessoa2 = new pessoa2 (matheus,18,front-end,2500)
- const pessoa3 = new pessoa3 (julio,30,motorista,1500)
+ class funcionario2 extends pessoa {
+    constructor (nome, idade, trabalho){
+        super(nome,idade,trabalho)
+        this.linguagem = linguagem;
+    }
+
+ }
+
+
+ const pessoa1 = new pessoa1 ("watillas" , 23, "back-end" , 3000 , "pyhton")
+ const pessoa2 = new pessoa2 ("matheus",18,"front-end",2500,"html")
+ const pessoa3 = new pessoa3 ("julio",30,"dev",5000,"js")
+
+ console.log(pessoa1);
+ console.log(pessoa2);
+ console.log(pessoa3);
